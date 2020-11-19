63105bfae9b33733c5933dbfce2123bb5f6c8537
TheSavageTeddy <TheSavageTeddy@users.noreply.github.com>
2020-11-19 14:25:08 +0800

    dimensions of terminal

--- snake.py
+++ snake.py
@@ -1,8 +1,9 @@
-import os
+import os, re
 
 try:
-    size = os.get_terminal_size()
+    height = re.search("(\d+)\)", str(os.get_terminal_size())).group(1)
+    width = re.search("(\d+),", str(os.get_terminal_size())).group(1)
 except OSError:
-    print("This game should be played in the terminal")
-    exit()
+    print("This game should be played in the terminal.")
+    # exit()
 
