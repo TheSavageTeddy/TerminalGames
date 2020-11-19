53fda3dd3a7b304dc5aa650c9c6252c577f89573
TheSavageTeddy <TheSavageTeddy@users.noreply.github.com>
2020-11-19 14:59:17 +0800

    stuff

--- snake.py
+++ snake.py
@@ -1,9 +1,36 @@
-import os, re
+import os, re, time
 
 try:
-    height = re.search("(\d+)\)", str(os.get_terminal_size())).group(1)
-    width = re.search("(\d+),", str(os.get_terminal_size())).group(1)
+    height = int(re.search("(\d+)\)", str(os.get_terminal_size())).group(1))-1 #the terminal needs -1 because uh
+    width = int(re.search("(\d+),", str(os.get_terminal_size())).group(1))
 except OSError:
     print("This game should be played in the terminal.")
     # exit()
 
+#
+#    ASCII
+#
+#    ┃
+#    ━
+#    ┛
+#    ┗
+#    ┓
+#    ┏
+#    ●
+#
+#    SNAKE EXAMPLE
+#
+#    ━━━━━━┓
+#        ┃
+#        ┃
+#        ●
+#
+
+
+speed = 0.4
+
+while True:
+    for i in range(0, height):
+        print("."*width)
+
+    time.sleep(speed)
