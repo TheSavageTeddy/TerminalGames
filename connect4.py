8694c60f70b3b5036a73b4a0e60318edc935a248
TheSavageTeddy <TheSavageTeddy@users.noreply.github.com>
2020-11-20 15:20:52 +0800

    connect 4 art

--- connect4.py
+++ connect4.py
@@ -1,1 +1,40 @@
 import os, re, time, random, keyboard, math
+
+os.system("stty -echo") #makes text entered in terminal invisible
+
+try:
+    height = int(re.search("(\d+)\)", str(os.get_terminal_size())).group(1))-1 #the terminal needs -1 because uh
+    width = int(re.search("(\d+),", str(os.get_terminal_size())).group(1))
+except OSError: #if not ran in terminal
+    print("This game should be played in the terminal.")
+    height = 15
+    width = 15
+    # exit()
+
+#
+#    ASCII
+#
+#    ┃
+#    ━
+#    ┛
+#    ┗
+#    ┓
+#    ┏
+# ┣ ╋ ━ ┫
+#┗ ┻ ━ ┛
+#┏ ┳ ━ ┓
+
+'''
+GRID
+
+  ━╋━━━━━━━━━━━━━━━━━━━━━╋━
+   ┃ ·  ·  ·  ·  ·  ·  · ┃
+   ┃ ·  ·  ·  ·  ·  ·  · ┃
+   ┃ ·  ·  ·  ·  ·  ·  · ┃
+   ┃ ·  ·  ·  ·  ·  ·  · ┃
+   ┃ ·  ·  ·  ·  ·  ·  · ┃
+   ┃ ·  ·  ·  ·  ·  ·  · ┃
+  ━╋━━━━━━━━━━━━━━━━━━━━━╋━
+
+'''
+
--- snake.py
+++ snake.py
@@ -5,8 +5,10 @@ os.system("stty -echo") #makes text entered in terminal invisible
 try:
     height = int(re.search("(\d+)\)", str(os.get_terminal_size())).group(1))-1 #the terminal needs -1 because uh
     width = int(re.search("(\d+),", str(os.get_terminal_size())).group(1))
-except OSError:
+except OSError: #if not ran in terminal
     print("This game should be played in the terminal.")
+    height = 15
+    width = 15
     # exit()
 
 #
